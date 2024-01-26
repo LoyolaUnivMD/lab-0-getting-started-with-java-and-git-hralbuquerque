4f7a687dbf1c4387a6b03b33d42824a121a2b8e3
heloisa_albuquerque <hralbuquerque@loyola.edu>
2024-01-26 10:06:42 -0500

    Added ascii

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,22 @@
 //HelloWorld.java
-
+// Programmers: Heloisa Albuquerque
+// Course:  CS 212, Mr. John
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: In class
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println("ooooo   ooooo ooooooooo.         .o.      \n" +
+                "`888'   `888' `888   `Y88.      .888.     \n" +
+                " 888     888   888   .d88'     .8\"888.    \n" +
+                " 888ooooo888   888ooo88P'     .8' `888.   \n" +
+                " 888     888   888`88b.      .88ooo8888.  \n" +
+                " 888     888   888  `88b.   .8'     `888. \n" +
+                "o888o   o888o o888o  o888o o88o     o8888o");
+
     }
 }
